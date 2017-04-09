636214de1671d81f5e4908a2c68f28858228e8e1
crissdua <crissdua@hotmail.com>
2017-04-09 12:19:10 -0600

    Carga Masiva 1

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(22, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Clientes"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Label8)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(4, 8)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(622, 139)
+        Me.Panel1.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(181, 50)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(18, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "ID"
+        '
+        'Label6
+        '
+        Me.Label6.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label6.Location = New System.Drawing.Point(-2, -2)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(18, 139)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "C4C"
+        Me.Label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(224, 47)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(22, 42)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 23)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Existencia Bodegas"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(22, 71)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(119, 23)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "Historial de Servicios"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(22, 100)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(119, 23)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Uso de Maquinas"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(631, 187)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,76 @@
+﻿Imports OASP_C.ExBodegaService
+Imports System.Linq
+Imports System.Net
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.IO
+Imports System.Xml.Linq
+Imports System.Xml
+Imports System.Collections
+Imports System
+Imports System.Net.Security
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports System.Security.Cryptography.X509Certificates
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim objetoEscribir2 As New C4C_SaldoClientesB.CreateResponse
+        Dim leerrequest2 As New C4C_SaldoClientesB.CreateRequest
+
+        Dim objetoEscribir As New C4C_SaldoClientesV.CreateResponse
+        Dim leerrequest As New C4C_SaldoClientesV.CreateRequest
+
+        Dim request As IF_SaldoClientesV.SaldoClientesVType = New IF_SaldoClientesV.SaldoClientesVType
+        Dim response As IF_SaldoClientesV.SaldoClientesVResponseType = New IF_SaldoClientesV.SaldoClientesVResponseType
+        Dim cliente As New IF_SaldoClientesV.ipostep_vP0010000101in_WCSX_comsapb1ivplatformruntime_INB_WS_CALL_SYNC_XPT_INB_WS_CALL_SYNC_XPTipo_procClient
+        cliente.ClientCredentials.UserName.UserName = "B1iruntime"
+        cliente.ClientCredentials.UserName.Password = "12345678"
+        response = cliente.ZSaldoClientesV(request)
+
+        leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync = New C4C_SaldoClientesB.Saldo_ClienteBSaldo_ClienteBCreateRequestMessage_sync
+        leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB = New C4C_SaldoClientesB.Saldo_ClienteBSaldo_ClienteBCreateRequest
+        objetoEscribir2.Saldo_ClienteBSaldo_ClienteBCreateConfirmation_sync = New C4C_SaldoClientesB.Saldo_ClienteBSaldo_ClienteBCreateConfirmationMessage_sync
+        leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Nombre = New C4C_SaldoClientesB.Text
+        leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Saldo = New C4C_SaldoClientesB.Text
+
+        leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync = New C4C_SaldoClientesV.Saldo_ClienteVSaldo_ClienteVCreateRequestMessage_sync
+        leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV = New C4C_SaldoClientesV.Saldo_ClienteVSaldo_ClienteVCreateRequest
+        objetoEscribir.Saldo_ClienteVSaldo_ClienteVCreateConfirmation_sync = New C4C_SaldoClientesV.Saldo_ClienteVSaldo_ClienteVCreateConfirmationMessage_sync
+        leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV.Nombre = New C4C_SaldoClientesV.Text
+        leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV.Saldo = New C4C_SaldoClientesV.Quantity
+
+
+        Dim cliente2 As New C4C_SaldoClientesV.YGGG2714Y_SaldoClienteVClient
+        cliente2.ClientCredentials.UserName.UserName = "_BODEGACS_1"
+        cliente2.ClientCredentials.UserName.Password = "Welcome08"
+
+        Dim cliente3 As New C4C_SaldoClientesB.Y3YVY3FOY_Saldo_ClienteBClient
+        cliente3.ClientCredentials.UserName.UserName = "_BODEGACS_1"
+        cliente3.ClientCredentials.UserName.Password = "Welcome08"
+        Try
+
+            For Each a As IF_SaldoClientesV.SaldoClientesVResponseTypeRow In response.SaldoClientesVResult
+                    leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV.Entry = a.CardCode
+                    leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV.Nombre.Value = a.CardName
+                    leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync.Saldo_ClienteV.Saldo.Value = a.Balance
+                'objetoEscribir.Saldo_ClienteVSaldo_ClienteVCreateConfirmation_sync = cliente2.Create(leerrequest.Saldo_ClienteVSaldo_ClienteVCreateRequest_sync)
+                If a.Balance > 0 Then
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Entry = a.CardCode
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Nombre.Value = a.CardName
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Saldo.Value = "TRUE"
+                Else
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Entry = a.CardCode
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Nombre.Value = a.CardName
+                    leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync.Saldo_ClienteB.Saldo.Value = "FALSE"
+                End If
+                'objetoEscribir2.Saldo_ClienteBSaldo_ClienteBCreateConfirmation_sync = cliente3.Create(leerrequest2.Saldo_ClienteBSaldo_ClienteBCreateRequest_sync)
+            Next
+            Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
